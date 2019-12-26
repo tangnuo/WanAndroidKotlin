317943cdc64b8d679e4cb2fd090f5d12f89dbc21
caoweijian <caoweijian@kedacom.com>
2019-12-26 14:34:21 +0800

    Initial commit

---
+++ library-ktx/src/main/java/com/caowj/mobile/library_ktx/Ktx.kt
@@ -0,0 +1,51 @@
+package com.caowj.mobile.library_ktx
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+/**
+ * <pre>
+ *     作者：Caowj
+ *     邮箱：caoweijian@kedacom.com
+ *     日期：2019/12/26 14:23
+ * </pre>
+ */
+
+class Ktx : ContentProvider() {
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
